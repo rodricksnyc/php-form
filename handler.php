61df7f34d2efc442a40d6b0af63c59cdb42fe3c0
Elizabeth <elizabeth@Elizabeths-iMac.local>
2020-10-06 19:53:38 -0400

    bullshit:

--- handler.php
+++ handler.php
@@ -21,6 +21,6 @@ $validator->field('message')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('pchelp@westat.com'); // ← Your email here
+$pp->sendEmailTo('CAHPS1@westat.com'); // ← Your email here
 
 echo $pp->process($_POST);
